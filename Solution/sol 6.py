f009c4c7a9004571f307c9d67662d133f7681113
21BCA01 <96808007+21BCA01@users.noreply.github.com>
2022-03-31 09:55:19 +0530

    Add files via upload

---
+++ Solution/sol 6.py
@@ -0,0 +1,20 @@
+a=int(input("enter any number:"))
+if(a==1):
+      print("it's monday")
+elif(a==2):
+    print("it's tuesday")
+elif(a==3):
+    print("it's wednesday")
+elif(a==4):
+    print("it's thursday")
+elif(a==5):
+    print("it's friday")
+elif(a==6):
+    print("it's saturday")
+elif(a==7):
+    print("it's sunday")
+else:
+    print("there are no days")
+
+
+    
